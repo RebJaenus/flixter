f4962239b316c7f356b3074f3c204f0db6716a60
RebJaenus <bberglund@comcast.net>
2019-06-09 01:45:05 +0000

    added Enroll button and disabled it if user already enrolled

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :courses
+  has_many :enrolled_courses, through: :enrollments, source: :course
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def enrolled_in?(course)
+    return enrolled_courses.include?(course)
+  end
 end
